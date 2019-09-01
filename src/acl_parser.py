f0a260832e7c4c60261b9347123231762821931f
gabe <gabeorlanski@gmail.com>
2019-09-01 15:38:07 -0400

    Added some arguments so no errors are thrown
    Need to implement them

--- src/acl_parser.py
+++ src/acl_parser.py
@@ -21,8 +21,10 @@ import warnings
 
 
 class ACLParser:
-    def __init__(self, xpath_config, save_data=False, save_path="/data", ext_directory=False, existing_data=None,file_log_level=logging.DEBUG,
+    def __init__(self, xpath_config=None, save_data=False, save_path="/data", ext_directory=False, existing_data=None,file_log_level=logging.DEBUG,
                  console_log_level=logging.ERROR, log_format=None, log_path=None, cores=4):
+        if xpath_config is None:
+            raise ValueError("no xpath_config was passed")
         self.aliases = {}
         self.same_name = []
         self.similar_names = {}
