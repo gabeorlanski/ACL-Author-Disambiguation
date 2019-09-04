d8af2af1bd8ed69b6dc58d20fc2bb177470b3683
gabriel-orlanski <gabeorlanski@gmail.com>
2019-09-04 15:31:51 -0400

    Finished input handler

--- disambiguate.py
+++ disambiguate.py
@@ -1,6 +1,7 @@
 from src.utility_functions import createCLIGroup, createCLIShared, createLogger, loadData
 from src.target_creator import TargetCreator
 from src.author_disambiguation import AuthorDisambiguation
+from src.input_handler import InputHandler
 from src.paper import Paper
 from src.config_handler import ConfigHandler
 import json
@@ -16,15 +17,22 @@ arguments = argparse.ArgumentParser(
 createCLIShared(arguments)
 createCLIGroup(arguments, "TargetCreator", "Arguments for how to create targets", TargetCreator.parameters)
 createCLIGroup(arguments, "AuthorDisambiguation", "Arguments for how to disambiguate authors, check author_disambiguation.py for default values",
-               TargetCreator.parameters)
+               AuthorDisambiguation.parameters)
 
 
 if __name__ == '__main__':
     args = arguments.parse_args()
     with open(os.getcwd() + "/logs/disambiguate.log", 'w'):
         pass
-    log_path = os.getcwd() + "/logs/create_data.log"
+    log_path = os.getcwd() + "/logs/disambiguate.log"
     print("INFO: Starting Create Data")
     gc.collect()
     config_raw = json.load(open("config.json"))
     config = ConfigHandler(config_raw, "disambiguate", raise_error_unknown=True)
+    data = loadData(["parsed_papers","author_papers","id_to_name"],config.logger,config)
+    papers = {x:Paper(**v) for x,v in data["parsed_papers"].items()}
+    author_papers = data["author_papers"]
+    id_to_name = data["id_to_name"]
+
+    input_handler = InputHandler(papers,author_papers,id_to_name, **config["InputHandler"])
+    input_handler.handleUserInput()
--- src/input_handler.py
+++ src/input_handler.py
@@ -9,7 +9,6 @@ remove_weird_notes = re.compile("\(\w+\)")
 
 
 class InputHandler:
-    parameters
     def __init__(self, papers, author_papers, id_to_name, console_log_level=logging.ERROR, file_log_level=logging.DEBUG, log_format=None,
                  log_path=None, target_path=None, save_data=False, ext_directory=False, save_path=None, cores=4):
         if not log_format:
@@ -23,7 +22,10 @@ class InputHandler:
         self.id_to_name = id_to_name
         self.names = defaultdict(list)
         for k, name in id_to_name.items():
-            self.names[cleanName(remove_weird_notes.sub(" ",name).replace("  "," "))].append(k)
+            if k == "yang-liu-georgetown":
+                print("DEBUG")
+            name_cleaned =cleanName(remove_weird_notes.sub(" ", nameFromDict(name)).replace("  ", " ")).replace("  "," ")
+            self.names[name_cleaned].append(k)
         self.save_data = save_data
         self.save_path = save_path
         self.ext_directory = ext_directory
@@ -60,42 +62,54 @@ class InputHandler:
             self.logger.debug("Found {} targets".format(len(self.targets)))
             self.logger.debug("Found {} overrides".format(len(self.override_authors)))
 
-        # commands in the format {command:{ required value, description}
         self.valid_main_commands = {
             "t": {
                 "required": "target-id",
-                "desc": "Specify target"
-            },
-            "m": {
-                "required": "target-id",
-                "desc": "Modify a target"
+                "desc": "Specify target",
+                "action": self._addTarget
             },
             "d": {
                 "required": None,
-                "optional": "target-id",
-                "desc": "Display a target or all targets"
+                "desc": "Display a target or all targets",
+                "action": self._displayTargets
             },
             "r": {
-                "required": "target-id",
-                "optional": None,
-                "desc": "Remove a target"
+                "required": None,
+                "desc": "Remove a target",
+                "action": self._removeTarget
+
             },
-            "v": {
-                "required": "paper-id",
-                "optional": None,
-                "desc": "View a paper"
+            "g": {
+                "required": "target-id",
+                "desc": "Generate authors to compare with based on their name",
+                "action": self._genAuthorOverride
+
+            },
+            "c": {
+                "required": "target-id",
+                "desc": "Clear target's authors to compare with",
+                "action": self._clearAuthorOverride
+
+            },
+            "o": {
+                "required": None,
+                "desc": "Display override authors",
+                "action": self._displayOverride
             },
             "h": {
                 "required": None,
-                "desc": "Help"
+                "desc": "Help",
+                "action": self._printHelp
             },
             "s": {
                 "required": None,
-                "desc": "Save targets"
+                "desc": "Save targets",
+                "action": self._save
             },
             "e": {
                 "required": None,
-                "desc": "Finish and continue"
+                "desc": "Finish and continue",
+                "action": None
             },
         }
         self.valid_target_commands = {
@@ -127,36 +141,53 @@ class InputHandler:
 
         }
 
-    def handleUserInput(self, user_input):
-        if user_input == "\n" or len(user_input) == 0:
-            self.logger.debug("Got empty user input")
-            return
+    def handleUserInput(self):
+        printLogToConsole(self.console_log_level, "Found {} targets".format(len(self.targets)), logging.INFO, logger=self.logger)
+        print("INFO: Enter target-ids:")
+        while 1:
+            user_input = input(">> ")
+            if user_input == "\n" or len(user_input) == 0:
+                self.logger.debug("Got empty user input")
 
-        command_split = user_input.split(" ")
-        command = command_split[0]
-        if command not in self.valid_main_commands:
-            printLogToConsole(self.console_log_level,"{} is not a valid command".format(command),logging.INFO,logger=self.logger)
-            return
-    
-    def _validAuthor(self,a):
+            command_split = user_input.split(" ")
+            command = command_split[0]
+            if command not in self.valid_main_commands:
+                printLogToConsole(self.console_log_level, "{} is not a valid command".format(command), logging.INFO, logger=self.logger)
+            elif command == "e":
+                if self.confirmAction():
+                    return
+            else:
+                action = self.valid_main_commands[command]["action"]
+                if self.valid_main_commands[command]["required"]:
+                    if len(command_split) != 2:
+                        print("Invalid arguments passed to {}".format(command))
+                    else:
+                        action(command_split[1])
+                else:
+                    action()
+                self.logger.debug("Finished running command {}".format(command))
+
+    def _validAuthor(self, a):
         self.logger.debug("Checking if {} is a valid author-id".format(a))
         if a not in self.author_papers:
-            self.logger.debug("{} is not in author_papers")
+            self.logger.debug("{} is not in author_papers".format(a))
             return -1
         at_least_one_parsed = False
-        for p in self.author_papers:
+        for p in self.author_papers[a]:
+            self.logger.debug("Checking paper {}".format(p))
             if p in self.papers:
                 at_least_one_parsed = True
         if not at_least_one_parsed:
-            self.logger.debug("{} has no parsed papers")
+            self.logger.debug("{} has no parsed papers".format(a))
             return -2
         return 0
 
     def _getAuthorInfo(self, a):
         self.logger.debug("Getting info for {}".format(a))
-        printLogToConsole(self.console_log_level,"id={}".format(a),logging.INFO,logger=self.logger)
-        printLogToConsole(self.console_log_level,"name={}".format(nameFromDict(self.id_to_name[a])),logging.INFO,logger=self.logger)
-        printLogToConsole(self.console_log_level,"Papers=",logging.INFO,logger=self.logger)
+        name = cleanName(remove_weird_notes.sub(" ", nameFromDict(self.id_to_name[a])).replace("  ", " ")).replace("  "," ")
+        printLogToConsole(self.console_log_level, "id={}".format(a), logging.INFO, logger=self.logger)
+        printLogToConsole(self.console_log_level, "name={}".format(name), logging.INFO, logger=self.logger)
+        printLogToConsole(self.console_log_level, "Papers for {}:".format(a), logging.INFO, logger=self.logger)
         for p in self.author_papers[a]:
             if p not in self.papers:
                 continue
@@ -164,7 +195,8 @@ class InputHandler:
                 title = self.papers[p].title
             except:
                 title = self.papers[p]["title"]
-            printLogToConsole(self.console_log_level, "--{}\t{}".format(p,title), logging.INFO, logger=self.logger)
+            printLogToConsole(self.console_log_level, "\t{}\t{}".format(p, title), logging.INFO, logger=self.logger)
+        printLogToConsole(self.console_log_level, "{} Author(s) have this name".format(len(self.names[name])), logging.INFO, logger=self.logger)
 
     @staticmethod
     def confirmAction():
@@ -174,3 +206,117 @@ class InputHandler:
                 return True
             elif confirmation.lower() == "n":
                 return False
+
+    def _addTarget(self, a):
+        self.logger.debug("Received add command with arguments {}".format(a))
+        valid_author = self._validAuthor(a)
+        if valid_author == -1:
+            printLogToConsole(self.console_log_level, "{} is not a valid author id".format(a), logging.INFO, logger=self.logger)
+        elif valid_author == -2:
+            printLogToConsole(self.console_log_level, "{} has no parsed papers".format(a), logging.INFO, logger=self.logger)
+        else:
+            self._getAuthorInfo(a)
+            self.targets.append(a)
+
+            if a not in self.override_authors:
+                printLogToConsole(self.console_log_level, "{} does not have specified authors to compare with".format(a), logging.INFO,
+                                  logger=self.logger)
+            else:
+                printLogToConsole(self.console_log_level, "{} has {} authors to compare with".format(a,len(self.override_authors[a])), logging.INFO,
+                                  logger=self.logger)
+            printLogToConsole(self.console_log_level, "{} added to targets".format(a), logging.INFO, logger=self.logger)
+            printLogToConsole(self.console_log_level, "{} current targets".format(len(self.targets)), logging.INFO, logger=self.logger)
+
+        return
+
+    def _removeTarget(self):
+        self.logger.debug("Received remove command")
+        print("INFO: Select the number of the id you would like to remove from targets, enter e to exit")
+        if len(self.targets) == 0:
+            printLogToConsole(self.console_log_level, "No possible targets to remove", logging.INFO, logger=self.logger)
+            return
+        while 1:
+            for i, v in enumerate(self.targets):
+                print("INFO: [{}] {}".format(i, v))
+            to_remove = input(">>")
+            if to_remove == "e":
+                self.logger.debug("Exit command received")
+                return
+            try:
+                to_remove = int(to_remove)
+            except ValueError:
+                printLogToConsole(self.console_log_level, "{} is not valid".format(to_remove), logging.INFO, logger=self.logger)
+                continue
+            if to_remove < 0:
+                printLogToConsole(self.console_log_level, "{} is not valid".format(to_remove), logging.INFO, logger=self.logger)
+                continue
+            elif to_remove >= len(self.targets):
+                printLogToConsole(self.console_log_level, "{} is not valid".format(to_remove), logging.INFO, logger=self.logger)
+                continue
+            else:
+                printLogToConsole(self.console_log_level, "{} is selected to be removed".format(self.targets[to_remove]), logging.INFO, logger=self.logger)
+                if self.confirmAction():
+                    self.targets.remove(self.targets[to_remove])
+                    printLogToConsole(self.console_log_level, "Remaining targets:", logging.INFO,
+                                      logger=self.logger)
+                    for i, v in enumerate(self.targets):
+                        print("INFO: {}".format(v))
+                    return
+                else:
+                    self.logger.debug("User did not confirm action")
+
+    def _printHelp(self):
+        self.logger.debug("Received help command")
+        print("INFO: Commands available:")
+        for k, v in self.valid_main_commands.items():
+            if v["required"]:
+                print("\t{} {:<10} {}".format(k, v["required"], v["desc"]))
+            else:
+                print("\t{:<12} {}".format(k, v["desc"]))
+
+    def _save(self):
+        self.logger.debug("Received save command")
+        return
+
+    def _displayTargets(self):
+        self.logger.debug("Received display command")
+        for a in self.targets:
+            if a in self.override_authors:
+                print("INFO: {:<20} {} papers and {} authors specified to compare with".format(a,len(self.author_papers[a]),len(self.override_authors[a])))
+            else:
+                print("INFO: {:<20} {} papers and {} authors specified to compare with".format(a,len(self.author_papers[a]),0))
+        return
+
+    def _clearAuthorOverride(self, a):
+        self.logger.debug("Received clear override command".format(a))
+        if a not in self.override_authors:
+            printLogToConsole(self.console_log_level, "{} does not have authors to compare with".format(a), logging.INFO, logger=self.logger)
+            return
+        if self.confirmAction():
+            del self.override_authors[a]
+            self.logger.debug("Removed {} from override_authors".format(a))
+
+    def _genAuthorOverride(self,a):
+        self.logger.debug("Received clear override command".format(a))
+        if a in self.override_authors:
+            printLogToConsole(self.console_log_level, "{} already has authors to compare with".format(a), logging.INFO, logger=self.logger)
+            return
+        elif a not in self.targets:
+            printLogToConsole(self.console_log_level, "{} is not a target".format(a), logging.INFO, logger=self.logger)
+            return
+        name = cleanName(remove_weird_notes.sub(" ", nameFromDict(self.id_to_name[a])).replace("  ", " ")).replace("  "," ")
+        print("INFO: Other authors with the same name:")
+        for other_a in self.names[name]:
+            if other_a !=a:
+                print("INFO: {}".format(other_a))
+        if len(self.names[name]) == 1:
+            printLogToConsole(self.console_log_level, "{} only has {}, will not add authors to compare with".format(name, a), logging.INFO, logger=self.logger)
+        else:
+            self.override_authors[a] = [x for x in self.names[name] if x != a]
+            self.logger.debug("{} authors added to override_authors".format(len(self.names[name])))
+            
+    def _displayOverride(self):
+        printLogToConsole(self.console_log_level, "Override Authors: ", logging.INFO,logger=self.logger)
+        for k in self.override_authors.keys():
+            printLogToConsole(self.console_log_level, "{} has {} authors to compare with".format(k, len(self.override_authors[k])), logging.INFO,
+                              logger=self.logger)
